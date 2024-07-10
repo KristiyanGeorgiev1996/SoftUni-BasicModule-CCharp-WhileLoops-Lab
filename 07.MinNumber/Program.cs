63c06955c3e894df09470e9e5d94631c976625f0
Kristiyan Georgiev <kristiqn35@yahoo.com>
2024-07-10 16:10:41 +0300

    Add files via upload

---
+++ 07.MinNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿string stop = Console.ReadLine();
+int min = int.MaxValue;
+
+while (stop != "Stop")
+{
+    int sum = int.Parse(stop);
+    if (sum < min)
+    {
+        min = sum;
+    }
+
+
+
+    stop = Console.ReadLine();
+}
+Console.WriteLine(min);
