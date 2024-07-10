f24199062fcbe7556c9d683e67387db992424a7e
Kristiyan Georgiev <kristiqn35@yahoo.com>
2024-07-10 16:09:14 +0300

    Add files via upload

---
+++ 05.AccountBalance/Program.cs
@@ -0,0 +1,17 @@
+﻿string input = Console.ReadLine();
+double total = 0;
+
+while (input != "NoMoreMoney")
+{
+    double sum = double.Parse(input);
+    if (sum < 0)
+    {
+        Console.WriteLine("Invalid operation!");
+        break;
+    }
+
+    total += sum;
+    Console.WriteLine($"Increase: {sum:F2}");
+    input = Console.ReadLine();
+}
+Console.WriteLine($"Total: {total:f2}");
