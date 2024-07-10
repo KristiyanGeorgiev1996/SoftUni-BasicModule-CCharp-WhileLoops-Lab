2bee9fc345fe57f280fdb49438baddf53ec2a311
Kristiyan Georgiev <kristiqn35@yahoo.com>
2024-07-10 16:08:13 +0300

    Add files via upload

---
+++ 03.SumNumbers/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = int.Parse(Console.ReadLine());
+int obshtaSuma = 0;
+
+
+
+while (obshtaSuma < n)
+{
+   int sum = int.Parse(Console.ReadLine());
+    obshtaSuma += sum;
+}
+Console.WriteLine(obshtaSuma);
