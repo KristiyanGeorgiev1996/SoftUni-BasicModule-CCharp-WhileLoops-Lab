9c0a8d2aee4f39e545492a94405adb5770fb20df
Kristiyan Georgiev <kristiqn35@yahoo.com>
2024-07-10 16:07:40 +0300

    Add files via upload

---
+++ 02.Password/Program.cs
@@ -0,0 +1,10 @@
+﻿string name = Console.ReadLine();
+string pass = Console.ReadLine();
+
+string vuvejdane = Console.ReadLine();
+
+while (vuvejdane != pass)
+{
+    vuvejdane = Console.ReadLine();
+}
+Console.WriteLine($"Welcome {name}!");
