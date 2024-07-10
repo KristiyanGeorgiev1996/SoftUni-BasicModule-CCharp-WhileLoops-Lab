008bf3fd5a8ed22cef2065953174dea32886bed0
Kristiyan Georgiev <kristiqn35@yahoo.com>
2024-07-10 16:10:16 +0300

    Add files via upload

---
+++ 06.MaxNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿
+string chislo = Console.ReadLine();
+int maxNumber = int.MinValue;
+
+while (chislo != "Stop")
+{
+    int sum = int.Parse(chislo);
+    if (maxNumber < sum)
+    {
+        maxNumber = sum;
+    }
+
+
+    chislo = Console.ReadLine();
+}
+
+Console.WriteLine(maxNumber);
