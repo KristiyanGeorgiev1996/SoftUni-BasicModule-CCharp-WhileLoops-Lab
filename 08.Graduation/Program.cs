b09cb72b89b3034171ea04e64f9b3fa4e65de0e5
Kristiyan Georgiev <kristiqn35@yahoo.com>
2024-07-10 16:11:16 +0300

    Add files via upload

---
+++ 08.Graduation/Program.cs
@@ -0,0 +1,29 @@
+﻿string name = Console.ReadLine();
+int klas = 1;
+int baddocenki = 0;
+double ocenkiobshto = 0;
+
+while (klas <= 12)
+{
+    double ocenka = double.Parse(Console.ReadLine());
+    if (ocenka <4.00)
+    {
+        baddocenki++;
+        if(baddocenki < 2)
+        {
+            continue;
+        }
+        else
+        {
+            Console.WriteLine($"{name} has been excluded at {klas} grade");
+            break;
+        }
+    }
+    klas++;
+    ocenkiobshto += ocenka; 
+}
+if (klas >= 12)
+{
+    double total = ocenkiobshto / 12;
+    Console.WriteLine($"{name} graduated. Average grade: {total:f2}");
+}
